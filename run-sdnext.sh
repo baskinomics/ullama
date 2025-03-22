0fd7eeea18ed7f39efded231bb6d22ed44b58f9f
Sean Baskin <seanbaskin@gmail.com>
2025-03-22 16:29:29 -0600

    Added installation path argument

--- run-sdnext.sh
+++ run-sdnext.sh
@@ -1,5 +1,4 @@
+#!/bin/bash
 export LD_PRELOAD=libtcmalloc.so.4
-
-cd ../sdnext/
-
+cd "$1" || { echo "Error: Could not enter directory '$1'"; exit 1; }
 ./webui.sh --debug --autolaunch --listen --uv
