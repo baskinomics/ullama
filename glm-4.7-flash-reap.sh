b526cfd55c0c003443efa1103e9b374cd4c363ba
Sean Baskin <seanbaskin@gmail.com>
2026-02-05 17:11:29 -0700

    Reduced context size for GLM 4.7 Flash REAP

--- glm-4.7-flash-reap.sh
+++ glm-4.7-flash-reap.sh
@@ -5,6 +5,6 @@ llama-server -hf unsloth/GLM-4.7-Flash-REAP-23B-A3B-GGUF:Q4_K_XL \
     --temp 0.7 \
     --top-p 1.00 \
     --min-p 0.01 \
-    --ctx-size 131072 \
+    --ctx-size 16384 \
     --port 8001 \
     --jinja
