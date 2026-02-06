8a224e7f1a8e4db9518e917359073b26acfd5999
Sean Baskin <seanbaskin@gmail.com>
2026-02-05 17:08:29 -0700

    Added update and upgrade flags to SDNext run file

--- run-sdnext.sh
+++ run-sdnext.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export LD_PRELOAD=libtcmalloc.so.4
 cd "$1" || { echo "Error: Could not enter directory '$1'"; exit 1; }
-./webui.sh --debug --autolaunch --listen --uv
+./webui.sh --debug --autolaunch --listen --uv --update --upgrade
