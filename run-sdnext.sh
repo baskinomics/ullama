333d1921d5123e1039eb9a257ddcf5ea324ed183
baskinomics <seanbaskin@gmail.com>
2026-02-28 19:02:37 -0700

    Removed SDNext runner script

--- run-sdnext.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export LD_PRELOAD=libtcmalloc.so.4
-cd "$1" || { echo "Error: Could not enter directory '$1'"; exit 1; }
-./webui.sh --debug --autolaunch --listen --uv --update --upgrade
